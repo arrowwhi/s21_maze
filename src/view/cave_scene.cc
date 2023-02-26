b46a427db59e05222103e33aa20f221439ff57de
Alexey <zam7@icloud.com>
2023-02-26 20:28:57 +0100

    all2

--- src/view/cave_scene.cc
+++ src/view/cave_scene.cc
@@ -14,7 +14,8 @@ void CaveScene::Create() {
   cave_->Create(ui_->caveCols->value(), ui_->caveRows->value(),
                 ui_->caveChance->value(), ui_->caveBirth->value(),
                 ui_->caveDeath->value());
-  SetSteps(ui_->caveCols->value(), ui_->caveRows->value());
+  // SetSteps(ui_->caveCols->value(), ui_->caveRows->value());
+  SetSteps(ui_->caveRows->value(), ui_->caveCols->value());
   Draw();
 }
 
@@ -23,7 +24,8 @@ void CaveScene::FromFile(std::string path) {
       cave_->FromFile(path, ui_->caveBirth->value(), ui_->caveDeath->value());
   ui_->caveRows->setValue(size.second);
   ui_->caveCols->setValue(size.first);
-  SetSteps(ui_->caveCols->value(), ui_->caveRows->value());
+  // SetSteps(ui_->caveCols->value(), ui_->caveRows->value());
+  SetSteps(ui_->caveRows->value(), ui_->caveCols->value());
   Draw();
 }
 
